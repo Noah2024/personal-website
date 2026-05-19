8fe3f76523fed927819575f97cddad1875babb92
Noah Yurasko <ndyurasko@gmail.com>
2026-05-19 15:42:03 -0400

    removing bad api key

--- test.sh
+++ test.sh
@@ -17,16 +17,3 @@ do
 
 done < includedProjects.csv
 
-
-
-
-# curl -L \
-#   -H "Accept: application/vnd.github.raw+json" \
-#   -H "Authorization: Bearer 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" \
-#   https://api.github.com/repos/Noah2024/422-tsiraM/readme?ref=master
-# curl -L https://raw.githubusercontent.com/Noah2024/422-tsiraM/master/README.md -o README.md
-
-# curl -L \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "X-GitHub-Api-Version: 2026-03-10" \
-#   https://api.github.com/repos/Noah2024/422-tsiraM/branches/master
