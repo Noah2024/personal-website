ded71eb790baa3c12bf3f3898195dee4fb3f8e42
Noah Yurasko <ndyurasko@gmail.com>
2026-05-20 16:06:04 -0400

    shitty card animation finished

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,57 @@ document.addEventListener('DOMContentLoaded', () => {
     // const cards = document.getElementsByClassName('card-container');
     const fullOver = document.getElementsByClassName('fullover')[0];
 
-    console.log(cards.length);
-
+    //Setting up cards to be animated correctly
      for (const card of cards) {
         
         card.addEventListener('focusin', () => {
             console.log("focousin")
             fullOver.classList.toggle('show');
+            expandAnimation(card)
         });
 
         card.addEventListener('focusout', () => {
             console.log("focousout")
             fullOver.classList.toggle('show');
+            expandAnimation(card)
+
         });
     }
 
+
 });
+
+function expandAnimation(card){
+    const first = card.getBoundingClientRect();
+    card.classList.toggle('expanded')
+    const last = card.getBoundingClientRect();
+
+    const dx = first.left - last.left
+    const dy = first.top - last.top
+    const dsx = first.width / last.width
+    const dsy = first.height /last.height
+
+    console.log(dx, dx, dsx, dsy)
+    // card.animate([], {})
+    //   keyframes -> [transform, scale, etc], properties -> {duration, east, etc}
+    card.animate([
+        //Frame One
+        {transformOrigin: "top left",
+         transform: `
+            translate(${dx}px, ${dy}px)
+            scale(${dsx}, ${dsy})
+         `
+        },
+        {
+            transform: `none`
+        }
+        //Frame Two
+        
+    ], {
+        duration: 500,
+        easing: "ease"
+    }).onfinish = () => {
+        console.log("Run on finsihed")
+    card.style.transform = "";
+  };
+}
