4033b313588bec6a8273232ceed06b60765f0486
Noah Yurasko <ndyurasko@gmail.com>
2026-06-03 13:03:24 -0400

    first pass at mobile compatability (really need to fix my fixed formatting sheme

--- webroot/templates/build.mjs
+++ webroot/templates/build.mjs
@@ -20,7 +20,7 @@ const html = `
 <head>
     <meta charset="UTF-8">
     <meta name="personal-website">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/static/main.css"> 
     <script src="./scripts/main.js" defer></script>
     <link rel="stylesheet" href="./templates/components/header/header.css">
--- webroot/templates/components/header/header.mjs
+++ webroot/templates/components/header/header.mjs
@@ -3,7 +3,7 @@ export default function Header() {
         return `
         <header>
             <nav class = "navmain"> 
-                <ul>
+                <ul class = "site-links">
                     <li> <a href="#home"> Home </a></li>
                     <li> <a href="#projects"> Projects </a> </li>
                     <li> <a href="#about"> About </a></li>
@@ -12,7 +12,7 @@ export default function Header() {
                     <li> <a href="#contact"> Contact</a> </li>
                 </ul>
 
-                <ul style="flex-direction: row-reverse ">
+                <ul class="outside-links" style="flex-direction: row-reverse ">
                     <li> <a href="#contact"> <img src="/static/images/topbar/download.svg"> </a> </li>
                     <li> <a href="https://www.linkedin.com/in/noah-yurasko-a86207336/"> <img src="/static/images/topbar/linkedin.svg"> </a></li>
                     <li> <a href="https://github.com/Noah2024"> <img src="/static/images/topbar/github.svg"> </a> </li>
