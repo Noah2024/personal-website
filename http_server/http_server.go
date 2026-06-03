434290081c7de12c5bc1481932407f69fe4713ed
Noah Yurasko <ndyurasko@gmail.com>
2026-06-03 17:25:33 -0400

    testing new api routes

--- http_server/http_server.go
+++ http_server/http_server.go
@@ -6,8 +6,7 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-<<<<<<< Updated upstream
+func handleApiRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello testing the url %s\n", r.URL.Path)
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -19,13 +18,25 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(bodyString)
 
 	// err := os.WriteFile("tmp.json", []byte(r.Body))
-=======
-	fmt.Fprint(w, "Hello testing (BIG TEST) the url %s\n", r.URL.Path)
->>>>>>> Stashed changes
+}
+
+func handleWebsiteUpdate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello testing the url %s\n", r.URL.Path)
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read body of request", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
 }
 
 func Start() {
-	http.HandleFunc("/", helloHandler)
+	//nginx proxy points indoshon.com/api twords the following root
+	//Making all other routes indoshon.com/api/route
+	http.HandleFunc("/", handleApiRoot)
+	http.HandleFunc("/update", handleWebsiteUpdate)
 
 	fmt.Println("Starting server at :8088")
 	if err := http.ListenAndServe(":8088", nil); err != nil {
