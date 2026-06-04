17980c7a67d18234dd7f3b4787f3bb678d26df82
Noah Yurasko <ndyurasko@gmail.com>
2026-06-04 12:47:14 -0400

    fixed spelling mistakes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # personal-website
 
-This repository contains the source code for my personal website project. It includes a basic Go HTTP server, frontend templates, static assets, and scripts used to include new projects, the sites index is compiled statically using node. All animations are done nateivly in html css and javascript. Github webhook configured to automatically update deployed instance of server at [indoshon.com](https://indoshon.com).
+This repository contains the source code for my personal website project. It includes a basic Go HTTP server, frontend templates, static assets, and scripts used to include new projects, the sites index is compiled statically using node. All animations are done natively in html css and javascript. Github webhook configured to automatically update deployed instance of server at [indoshon.com](https://indoshon.com).
 
 ## Overview
 
@@ -16,4 +16,4 @@ This repository contains the source code for my personal website project. It inc
 
 - Template and static files are organized under `templates/` and `static/` for site rendering.
 - `includedProjects.csv` may be used to control which projects are published or listed. Only adds new projects, does not remove ones already listed, that is still done manually
-- Currently the one demo for tsiram is built dynamically at runtime, rather than statically. That is becuase I didn't want to deal with compiling wasm into the final static version. 
+- Currently the one demo for tsiram is built dynamically at runtime, rather than statically. That is because I didn't want to deal with compiling wasm into the final static version. 
--- webroot/templates/build.mjs
+++ webroot/templates/build.mjs
@@ -1,22 +1,16 @@
 import fs from "fs";
 import { projectGenerator } from "./components/projectGenerator/projectgen.mjs";
-//import pkg from './components/projectGenerator/projectgen.mjs';
-//const { projectGenerator } = pkg;
 import Header from "./components/header/header.mjs";
-//import pkg2 from './components/header/header.mjs';
-//const {header } = pkg2;
 import Footer from "./components/footer/footer.mjs";
-//import pkg3 from './components/footer/footer.mjs';
-//const {footer } = pkg3;
 
-//janky, but allws us to run from sh scrips in other directories we always run from this directory
+// janky, but allows us to run from sh scripts in other directories; we always run from this directory
 process.chdir(`${process.cwd()}/webroot`);
 
-const [cardHtml, expandedCardHtml] = projectGenerator()
+const [cardHtml, expandedCardHtml] = projectGenerator();
 
 const html = `
 <!DOCTYPE html>
-<html lan="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="personal-website">
@@ -32,7 +26,7 @@ const html = `
 </head>
 
 <body>
-    <!-- fullscreen blurr overlay -->
+    <!-- fullscreen blur overlay -->
     <div id="fullover" class="fullover"> </div>
     <!-- fullscreen card overlay -->
     <div class="card-overlay">
@@ -43,9 +37,9 @@ const html = `
     <main> 
         <h1 id="home"> INDOSHON.com </h1>
         <div class="aboutme-container">
-            <p> Welcome friends, family, countrymen, (potential employeers), welcome to indoshon.com.
-            The personal website mananged by and for Noah (indoshon) Yurasko, enjoy! (and if you thinking 'now who tf actually is Noah', luckily there is something called an about me section)
-            Please puruse the few personal projects (one or two from groups) which I actually am proud of. 
+            <p> Welcome friends, family, countrymen, (potential employers), welcome to indoshon.com.
+            The personal website managed by and for Noah (indoshon) Yurasko, enjoy! (and if you're thinking 'now who tf actually is Noah', luckily there is something called an about me section)
+            Please peruse the few personal projects (one or two from groups) which I actually am proud of. 
             </p>
         </div>
        
@@ -57,29 +51,29 @@ const html = `
         <h2 id="about"> About </h2>
         <div class="aboutme-container">
             <p> 
-            I am a Dual Major Junior at Marist University, looking to find expierences upon which I can build upon my programming and mathmatics expierence. 
-            My interest in Computer Science as a topic of study stemms from my facination by the seemily infinitley complex digital machines that have become common place in our daily lives. 
-            I've always had a desire to understand and even create these systems on my own, its this desire to unlock the unknown is why I've recently added a mathamtics double major. 
+            I am a Dual Major Junior at Marist University, looking to find experiences upon which I can build upon my programming and mathematics experience. 
+            My interest in Computer Science as a topic of study stems from my fascination with the seemingly infinitely complex digital machines that have become commonplace in our daily lives. 
+            I've always had a desire to understand and even create these systems on my own; it's this desire to unlock the unknown that is why I've recently added a mathematics double major. 
             <br> <br>
 
-            I am a very staunch supporter of the right to repair and firmly oppose the way in tech is currently being used to mine data on individuals for advertising and dynamic pricing. 
-            Most recently I've taken an interest in learning golang as well as ardunio, for which I will make projects im proud of sometime soon. 
+            I am a very staunch supporter of the right to repair and firmly oppose the way tech is currently being used to mine data on individuals for advertising and dynamic pricing. 
+            Most recently I've taken an interest in learning Golang as well as Arduino, for which I will make projects I'm proud of sometime soon. 
         
             </p>
         </div>
 
-        <h2 id=aipolicy> AI Policy </h2> 
+        <h2 id="aipolicy"> AI Policy </h2> 
         <div class="aboutme-container">
             <p> 
                 My personal AI Policy is quite simple, never allow AI to think for you. 
                 This manifests in many different use cases, such as finding documentation and syntax that may be obscure or hard to find, or 
                 bouncing ideas when working in spaces and with codebases I am not familiar. 
-                It's because of this policy I have not significantly explored AI-Driven workflows such as Claude Code, I personally always like to be in the driver's seat, 
+                It's because of this policy I have not significantly explored AI-driven workflows such as Claude Code; I personally always like to be in the driver's seat, 
                 even when working with AI tooling.  
 
                 <br><br> 
-                This policy is rooted in the belief that it's a REALLY BAD THING to allow all human knowledge to become utility bought and sold by big tech 
-                Cause seriously, having our ability to think slowly degraded then sold back to us just so some rich ass hole can be even more rich is genuinely terrifying. 
+                This policy is rooted in the belief that it's a REALLY BAD THING to allow all human knowledge to become a utility bought and sold by big tech. 
+                Cause seriously, having our ability to think slowly degraded then sold back to us just so some rich asshole can become even richer is genuinely terrifying. 
             </p>
         </div>
        
@@ -96,9 +90,5 @@ const html = `
 
 </body>
 </html>
-`
+`;
 fs.writeFileSync("./index.html", html);
-{/* <div class="aboutme-container"> 
-            <h1> Welcome! </h2>
-            <p> Welcome friends, family, countrymen, (potential employeers...), welcome to indoshon.com. 
-        </div> */}
